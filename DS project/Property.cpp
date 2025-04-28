3233b0688dbee1a63b7b3ffbd51655c4eb895391
abdelrahman1423 <za9223344565677@gmail.com>
2025-04-28 21:06:07 +0300

    Update Property.cpp

--- DS project/Property.cpp
+++ DS project/Property.cpp
@@ -1,1 +1,42 @@
 #include "Property.h"
+#ifndef PROPERTY_H
+#define PROPERTY_H
+
+#include <iostream>
+#include <vector> 
+#include "../../../json-develop/include/nlohmann/json.hpp"
+using namespace std;
+using json = nlohmann::json;
+
+class Property {
+public:
+    int id;
+    string name;
+    string ownerName;
+    string location;
+    double price;
+    vector<string> features;
+    double space;
+    string addedDate;
+    string condition; // For Sale or For Rent
+    string dealStatus; // Open or Closed
+
+public:
+    // Constructor
+    Property(int id, string name, string location, double price, vector<string> features,
+             double space, string addedDate, string condition, string dealStatus);
+
+    // Display property details
+    void display() const;
+
+    // Check if property has a specific feature
+    bool hasFeature(const string& feature) const;
+
+    // Get deal status
+    std::string getDealStatus() const;
+
+    // Convert property to JSON
+    json toJSON() const;
+};
+
+#endif
